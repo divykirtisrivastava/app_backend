406a2e9f77ee2794735759ebfb902d4d0281b8e4
divykirtisrivastava <divykirtisrivastava@gmail.com>
2024-10-02 13:52:49 +0530

    added

---
+++ main.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const mysql = require('mysql')
+const cors = require('cors')
+
+let app = express()
+app.use(express.json())
+app.use(cors())
+
+let db = mysql.createConnection({
+    database:'pashuapp',
+    host:'localhost',
+    user:'root'
+})
+db.connect((err)=>{
+    if(err) throw err
+    else{
+        console.log("database connected")
+    }
+})
+
+app.post('/save', (req, res)=>{
+    let email = req.body.email
+    let pasword = req.body.pasword
+    console.log(email)
+    let value = [[email, pasword]]
+    db.query('insert into user(email, password) values ?', [value], (err, result)=>{
+        if(err) throw err
+        else{
+            res.send("saved")
+        }
+    })
+})
+app.get('/getdata', (req, res)=>{
+    db.query('select * from user',(err, result)=>{
+        if(err) throw err
+        else{
+            res.json(result)
+        }
+    })
+})
+app.listen(3000, ()=>{
+    console.log("server is running")
+})
